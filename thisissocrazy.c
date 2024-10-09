9131c3b4b2210e59f4c5559e0c239b8b0c5e4ccf
adnene <bendiouisadnane3@gmail.com>
2024-10-09 16:22:24 +0100

    this is the craziest thing that ever happend to everything looks normal but when i try to do pow(c,2) with c = 5 it gives me 24 wth

---
+++ thisissocrazy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int ispythagore(int a, int b, int c){
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = pow(c, 2);
+    printf("%d\n", a);
+    printf("%d\n", b);
+    // a += b;
+    printf("%d\n", c);
+    if (c == a) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main(){
+    int a = 3;
+    int b = 4;
+    int c = 5;
+    int result = ispythagore(a, b, c);
+    printf("%d", result);
+    return 0;
+}
