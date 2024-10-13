fac9b0ee47e8c1ce8bedf20c34c265e41c86a5ab
adnene <bendiouisadnane3@gmail.com>
2024-10-13 15:51:22 +0100

    Refactor code for checking if a number is equal to the sum of its digits cubed

--- TP01/exo02.c
+++ TP01/exo02.c
@@ -2,49 +2,71 @@
 #include <math.h>
 
 int leurChiffres(int n){
-    int a,b,c,d;
-a=n/100;
-b=n%100;
-c=b/10;
-d=b%10;
-return(n==pow(a, 3) + pow(c, 3) + pow(d,3));
-    }
+    int a, b, c, d;
+    a = n / 100;
+    b = n % 100;
+    c = b / 10;
+    d = b % 10;
+    return (n == pow(a, 3) + pow(c, 3) + pow(d, 3));
+}
+
 void forloop(){
-    int i=140;
-    for(i=140;i<500;i++){
-    if(leurChiffres(i)){
-        printf("%d\n",i);
-    }
+    int i = 140;
+    for(i = 140; i < 500; i++){
+        if(leurChiffres(i)){
+            printf("%d\n", i);
+        }
     }
 }
+
 void whileloop(){
-    int i=140;
-    while(i<500){
+    int i = 140;
+    while(i < 500){
         if(leurChiffres(i)){
-            printf("%d\n",i);
+            printf("%d\n", i);
         }
         i++;
     }
 }
+
 void dowhileloop(){
-    int i=140;
+    int i = 140;
     do{
         if(leurChiffres(i)){
-            printf("%d\n",i);
+            printf("%d\n", i);
         }
         i++;
-    }while(i<(500));
+    } while(i < 500);
 }
 
-
-
-
 int main(){
+    int choice;
+    do {
+        printf("Menu:\n");
+        printf("1) For loop\n");
+        printf("2) While loop\n");
+        printf("3) Do-while loop\n");
+        printf("0) Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-forloop();
+        switch(choice) {
+            case 1:
+                forloop();
+                break;
+            case 2:
+                whileloop();
+                break;
+            case 3:
+                dowhileloop();
+                break;
+            case 0:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while(choice != 0);
 
-whileloop();
-dowhileloop();
-    
-return 0;
+    return 0;
 }
