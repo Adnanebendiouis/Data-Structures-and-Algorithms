2a84345d096b1b24c9073266f84c35c2ff302235
adnene <bendiouisadnane3@gmail.com>
2024-10-16 12:32:10 +0100

    Refactor findPythagoreanTriplets function to start with c=3 and b=2 for improved efficiency

--- TD01/exo01.c
+++ TD01/exo01.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
-void findPythagoreanTriplets(int N) {
+void findPythagoreanTriplets(int N)
+{
     int count = 0;
-    for (int c = 1; count < N; c++) {
-        for (int b = 1; b < c; b++) {
-            for (int a = 1; a < b; a++) {
-                if (a * a + b * b == c * c) {
+    for (int c = 3; count < N; c++)
+    {
+        for (int b = 2; b < c; b++)
+        {
+            for (int a = 1; a < b; a++)
+            {
+                if (a * a + b * b == c * c)
+                {
                     count++;
                     printf("%d: %d^2 + %d^2 = %d^2 (= %d)\n", count, a, b, c, c * c);
-                    if (count == N) {
-                        return;
-                    }
                 }
             }
         }
     }
 }
 
-int main() {
+int main()
+{
     int N;
     printf("Enter the number of Pythagorean triplets to display: ");
     scanf("%d", &N);
