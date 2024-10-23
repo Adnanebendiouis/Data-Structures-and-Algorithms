56efb8ae3f63350db73b212fb6378c3691cd24a6
adnene <bendiouisadnane3@gmail.com>
2024-10-23 12:35:34 +0100

    Refactor chercheTab function to fix indexing bug and improve efficiency

--- TD01/exo03.c
+++ TD01/exo03.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int chercheTab(int A[], int T[], int n, int m)
 {
-    if (n > m)
-    {
-        n = m;
-    }
+
     int count = 0;
     for (int i = 0; i < n; i++)
     {
-
-        if (A[i] == T[i])
+        for (int j = 0; j < m; j++)
         {
-            printf("%d  |", A[i]);
-            count++;
+            if (A[i] == T[j])
+            {
+                count++;
+                printf("%d |",A[i]);
+            }
         }
     }
     return count;
@@ -20,9 +19,9 @@ int chercheTab(int A[], int T[], int n, int m)
 
 int main()
 {
-    int A[] = {1, 2, 3};
-    int T[] = {1, 2, 3, 4, 5};
-    printf("Le nombre d'element est : %d", chercheTab(A, T, 4, 2));
+    int A[] = {1, 3, 7};
+    int T[] = {1, 2, 3, 7, 5};
+    printf("\nLe nombre d'element est : %d\n", chercheTab(A, T, 3, 5));
 
     return 0;
 }
